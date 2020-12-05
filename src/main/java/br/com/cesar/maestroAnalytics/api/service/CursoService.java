7e06fede47275a04b6c253d1a5856b33104b03d5
Rayssa Neder <rayssafreitas13@gmail.com>
2020-12-05 14:30:23 -0300

    bug cadastro de curso

--- src/main/java/br/com/cesar/maestroAnalytics/api/service/CursoService.java
+++ src/main/java/br/com/cesar/maestroAnalytics/api/service/CursoService.java
@@ -2,9 +2,13 @@ package br.com.cesar.maestroAnalytics.api.service;
 
 import java.util.Optional;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.logging.log4j2.Log4J2LoggingSystem;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 import br.com.cesar.maestroAnalytics.api.model.Curso;
 import br.com.cesar.maestroAnalytics.api.model.Instituicao;
@@ -15,6 +19,8 @@ import br.com.cesar.maestroAnalytics.api.repository.InstituicaoRepository;
 @Service
 public class CursoService {
 	
+	private static Log log = LogFactory.getLog(CursoService.class);
+	
 	@Autowired
 	private CursoRepository cursoRepository;
 	
@@ -23,13 +29,15 @@ public class CursoService {
 	
 	@Transactional
 	public Curso save(Curso curso) {
+		
 		Optional<Instituicao> instituicaoExistente = instituicaoRepository.findByCodigo(curso.getInstituicao().getCodigo());
 		
 		  if(!instituicaoExistente.isPresent()) {
 			  System.out.println("NÃO EXISTE INSTITUICAO COM O CODIGO " + curso.getInstituicao().getCodigo());
+			  log.error("Erro de sistema- NÃO EXISTE INSTITUICAO COM O CODIGO: " + curso.getInstituicao().getCodigo());
 		  }
 		 
-		  System.out.println("INSTITUICAO " + curso.getInstituicao().getCodigo());
+		  log.error("EXISTE sim INSTITUICAO COM O CODIGO: " + curso.getInstituicao().getCodigo());
 		  
 		curso.setInstituicao(instituicaoExistente.get());
 		
